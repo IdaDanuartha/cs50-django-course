0d80ea20e4a45c3c334cb96c49a47968eb81e1fa
IdaDanuartha <danuart21@gmail.com>
2023-03-26 22:25:32 +0800

    learn view, url and template

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("profile/<str:name>", views.profile, name="profile"),
+    path("newyear", views.newyear, name="newyear"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,21 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "home/index.html")
+
+def about(request):
+    return HttpResponse("Hi, its me danu!")
+
+def profile(request, name):
+    return render(request, "home/profile.html", {
+        "name": name.capitalize()
+    })
+
+def newyear(request):
+    is_newyear = datetime.datetime.now()
+    return render(request, "home/newyear.html", {
+        "is_newyear": is_newyear.month == 1 and is_newyear.day == 1
+    })
